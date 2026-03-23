7204cc6c2b891e89c83d0a3c59cfbc96abb30cfb
Sankalan Paul Chowdhury <53177253+sankalan4198@users.noreply.github.com>
2026-03-23 19:34:37 +0530

    Update InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -28,3 +28,32 @@ public class InsertionSort {
         }
     }
 }
+_----------------------------------------------
+    //Strivers method
+
+public class Main
+{
+	public static void main(String[] args) {
+		int arr[]={12,4,1,6};
+		for(int i=0;i<arr.length;i++)
+		{
+		    int j=i;
+		    
+		    while(j>0 && arr[j-1]>arr[j])
+		    {
+		        int temp=arr[j-1];
+		        arr[j-1]=arr[j];
+		        arr[j]=temp;
+		        j--;
+		        
+		    }
+		}
+		
+		
+		
+		for(int i=0;i<arr.length;i++)
+		{
+		    System.out.print(arr[i]+" ");
+		}
+	}
+}
